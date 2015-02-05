7dc3691f011c2a1b827114c19269c1c3644e84a0
Germain Boue <boue.germain@gmail.com>
2015-02-04 21:34:21 -0800

    Add validations, config ImageUploader in model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ActiveRecord::Base
+  mount_uploader :image, ImageUploader
+
+  validates_presence_of :name, :price
+  # validates price is a number
+  validates_numericality_of :price
 end
