30b3407668763290a4a73db37cfeee68bdc2010e
Germain Boue <boue.germain@gmail.com>
2015-02-08 12:15:01 -0800

    Fix bug

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,7 +2,21 @@ class Cart
 
 attr_reader :items
 
-def initialize
+
+def self.build_from_hash hash 
+  items = if hash["cart"] then 
+    hash["cart"]["items"].map do |item_data|
+      CartItem.new item_data["product_id"], item_data["quantity"]
+    end
+  else
+    []
+  end
+
+  new items
+end
+
+
+def initialize items = []
   @items = []
 end
 
